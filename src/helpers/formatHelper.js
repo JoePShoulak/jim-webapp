8cdbb73febede5927ce31197c3c8544739c9da4c
Joe P Shoulak <joepshoulak2@gmail.com>
2020-10-18 14:49:44 -0500

    Added keys to table rows and items to dodge error

--- src/helpers/formatHelper.js
+++ src/helpers/formatHelper.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-const formatTableRows = (table) => (
-     table.map(row => <tr>{row.map(item => <td>{item}</td>)}</tr>)
-)
+const formatTableRows = (table) => {
+    let rowKey = 0;
+    let itemKey = 0;
+
+    return (
+        table.map(row => {
+            return <tr key={rowKey++}>{row.map(item => {
+                return <td key={itemKey++}>{item}</td>
+            }) }</tr>
+        })
+    )
+}
 
 const DisplayTable = (table, validArray) => {
     let response;
